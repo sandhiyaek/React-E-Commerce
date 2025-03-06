37e2300188f6242b81c4935590987a02da264804
sandhiyaek <sandhiyaek589@gmail.com>
2025-03-06 11:36:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Contact from "./Components/Contact";
+import Home from "./Components/Home";
+import Loginform from "./Components/Loginform";
+import Privacy from "./Components/Privacy";
+import Signup from "./Components/Signup";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/loginform" element={<Loginform />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
